488c8e3bd99c9cf01a799296812114e4da6e0521
ArpitaGanatra <ganatraarpita@gmail.com>
2023-09-20 18:39:59 +0530

    feat: next auth

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+
+let isConnected = false;
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("mongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONOGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
